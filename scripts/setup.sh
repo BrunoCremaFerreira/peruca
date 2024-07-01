31a89c98f7eeba3230bab9ba634cdb10c289eef3
Bruno Crema Ferreira <eng.brn@gmail.com>
2024-07-01 20:55:38 -0300

    Script adjusts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,24 +3,39 @@
 # Virtual Environment
 VENV_DIR="venv"
 
-# Check if the virtual environment exist
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating Virtual Environment..."
-    python3 -m venv $VENV_DIR
-else
-    echo "Virtual Environment already exist."
-fi
-
-# Activate Virtual Environment
-source $VENV_DIR/bin/activate
-
 # Dependency install
-if [ -f "requirements.txt" ]; then
-    echo "Installing Dependencies..."
-    pip install -r requirements.txt
-else
-    echo "The file requirements.txt was not found!"
-    exit 1
-fi
+API_REQUIREMENTS_FILE="../src/api/requirements.txt"
 
-echo "Virtual Environment configured!"
+_create_venv()
+{
+    # Check if the virtual environment exist
+    if [ ! -d "$VENV_DIR" ]; then
+        echo "Creating Virtual Environment..."
+        python3 -m venv $VENV_DIR
+    else
+        echo "Virtual Environment already exist."
+    fi
+
+    # Activate Virtual Environment
+    source $VENV_DIR/bin/activate
+}
+
+_install_dependency()
+{
+    if [ -f "$1" ]; then
+        echo "Installing Dependencies..."
+        pip install -r "$1"
+    else
+        echo "The file "$1" was not found!"
+        exit 1
+    fi
+}
+
+_main()
+{
+    _create_venv
+    _install_dependency "$API_REQUIREMENTS_FILE"
+    echo "Virtual Environment configured!"
+}
+
+_main
