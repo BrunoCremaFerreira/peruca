7d5b2b727e80b09395e5cbd759d743189d9ef40f
Bruno Crema Ferreira <eng.brn@gmail.com>
2024-07-29 01:59:47 -0300

    adjusts

--- src/adapters/outbound/providers/openai_provider.py
+++ src/adapters/outbound/providers/openai_provider.py
@@ -1,14 +1,13 @@
 from openai import AsyncOpenAI
-
-aclient = AsyncOpenAI(api_key=self.api_key)
 from domain.ports.outbound.llm_outbound_port import LLMOutboundPort
 
 class OpenAIProvider(LLMOutboundPort):
     def __init__(self, api_key: str):
         self.api_key = api_key
+        self.aclient = AsyncOpenAI(api_key=self.api_key)
 
     async def generate_response(self, prompt: str) -> str:
-        response = await aclient.chat.completions.create(model="gpt-3.5-turbo",
+        response = await self.aclient.chat.completions.create(model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
         max_tokens=150)
         return response['choices'][0]['message']['content'].strip()
