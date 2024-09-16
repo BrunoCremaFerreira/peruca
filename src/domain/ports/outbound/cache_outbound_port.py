f0f474b7499a8a2897b8bdc1d074b3fb995e260f
Bruno Crema Ferreira <eng.brn@gmail.com>
2024-09-15 21:17:56 -0300

    Cache outbound

---
+++ src/domain/ports/outbound/cache_outbound_port.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class CacheOutboundPort(ABC):
+    """
+    Interface for cache operations.
+    """
+
+
+    @abstractmethod
+    async def set_key(self, key: str, value: str):
+        """
+        Stores a value associated with a key.
+        """
+        pass
+
+
+    @abstractmethod
+    async def get_key(self, key: str) -> str:
+        """
+        Retrieves the value associated with a key.
+        """
+        pass
+
+
+    @abstractmethod
+    async def delete_key(self, key: str):
+        """
+        Deletes a key from the cache.
+        """
+        pass
+
+
+    @abstractmethod
+    async def update_key(self, key: str, value: str):
+        """
+        Deletes a key from the cache.
+        """
+        pass
--- src/domain/ports/outbound/llm_outbound_port.py
+++ src/domain/ports/outbound/llm_outbound_port.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
 
 class LLMOutboundPort(ABC):
+    """
+    Interface for LLM Provider
+    """
+
+
     @abstractmethod
     async def generate_response(self, prompt: str) -> str:
+        """
+        Generates a response based on the given prompt.
+        """
         pass
