31134c69b97f15d60aeaf89836e1825e7ae5df17
Bruno Crema Ferreira <eng.brn@gmail.com>
2024-11-03 20:48:31 -0300

    swagger adjusts

--- src/api/main.py
+++ src/api/main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 api = Api(
     app,
     version="1.0",
-    title="API de Clientes e Pedidos",
-    description="Uma API para gerenciar clientes e pedidos",
+    title="Peruca LLM Assistant API",
+    description="",
 )
 
 
