50f1a2b5e46549f708d235c22f2e764c9b125a35
Bruno Crema Ferreira <eng.brn@gmail.com>
2024-11-03 20:35:34 -0300

    refactoring src

--- src/application/use_cases/chat_use_case.py
+++
@@ -1,9 +0,0 @@
-from domain.services.llm_service import LLMService
-
-
-class ChatUseCase:
-    def __init__(self, llm_service: LLMService):
-        self.llm_service = llm_service
-
-    async def execute(self, prompt: str) -> str:
-        return await self.llm_service.chat(prompt)
--- src/domain/exceptions.py
+++
@@ -1,2 +0,0 @@
-class InvalidEnvironmentSettingsError(ValueError):
-    pass
