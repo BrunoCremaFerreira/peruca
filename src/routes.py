68b4886cf9a8cdc5987793618b3762c686063c2c
Bruno Crema Ferreira <eng.brn@gmail.com>
2025-07-20 13:14:01 -0300

    Shopping list Routes

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from application.appservices.llm_app_service import LlmAppService
 from application.appservices.shopping_list_app_service import ShoppingListAppService
 from application.appservices.user_app_service import UserAppService
-from application.appservices.view_models import ChatRequest, ChatResponse, ShoppingListItemAdd, ShoppingListItemResponse, ShoppingListItemUpdate, UserAdd, UserResponse, UserUpdate
+from application.appservices.view_models import ChatRequest, ChatResponse, ShoppingListCleanType, ShoppingListItemAdd, ShoppingListItemResponse, ShoppingListItemUpdate, UserAdd, UserResponse, UserUpdate
 from infra.ioc import get_llm_app_service, get_shopping_list_app_service, get_user_app_service
 
 router = APIRouter()
@@ -58,21 +58,50 @@ def user_update(request: UserUpdate,
 # =====================================
 
 @router.get("/shopping-list", tags=["Shopping List"])
-def shopping_list_get_all(shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)) -> List[ShoppingListItemResponse]:
+def shopping_list_get_all(
+        shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+                          ) -> List[ShoppingListItemResponse]:
     return shopping_list_app_service.get_all()
 
 @router.get("/shopping-list/{id}", tags=["Shopping List"])
 def shopping_list_get(id: str,
-             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)) -> ShoppingListItemResponse:
+             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+             ) -> ShoppingListItemResponse:
     return shopping_list_app_service.get_by_id(item_id=id)
 
 @router.post("/shopping-list", tags=["Shopping List"])
 def shopping_list_add(request: ShoppingListItemAdd,
-             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)) -> dict:
+             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+             ) -> dict:
     item_id = shopping_list_app_service.add(item=request)
     return {"shopping_list_id": item_id}
 
 @router.put("/shopping-list", tags=["Shopping List"])
 def shopping_list_update(request: ShoppingListItemUpdate,
-                shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)) -> None:
+                shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+                ) -> None:
     shopping_list_app_service.update(item=request)
+
+@router.delete("/shopping-list/{id}", tags=["Shopping List"])
+def shopping_list_update(id: str,
+                shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+                ) -> None:
+    shopping_list_app_service.delete(item_id=id)
+
+@router.post("/shopping-list/clear/{clean_type}", tags=["Shopping List"])
+def shopping_list_clear(clean_type: ShoppingListCleanType,
+             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+             ) -> None:
+    shopping_list_app_service.clear(clean_type=clean_type)
+
+@router.put("/shopping-list/{id}/check", tags=["Shopping List"])
+def shopping_list_ckeck(id: str,
+             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+             ) -> None:
+    shopping_list_app_service.check(item_id=id)
+
+@router.put("/shopping-list/{id}/uncheck", tags=["Shopping List"])
+def shopping_list_ckeck(id: str,
+             shopping_list_app_service: ShoppingListAppService = Depends(get_shopping_list_app_service)
+             ) -> None:
+    shopping_list_app_service.uncheck(item_id=id)
